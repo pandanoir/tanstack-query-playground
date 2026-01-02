370d6e3a8687f81e22ee45210a36a337039aea9f
pandanoir <pandanoir@ymail.ne.jp>
2026-01-02 11:55:17 +0900

    課題 5：追加(POST)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useState } from 'react';
 import * as v from 'valibot';
 
@@ -50,6 +50,21 @@ function Post({ id }: { id: string }) {
   );
 }
 
+const createPost = async (title: string) => {
+  const res = await fetch('http://localhost:3000/posts', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      title,
+      views: 0,
+    }),
+  });
+  if (!res.ok) {
+    throw new Error('Request failed');
+  }
+  return await res.json();
+};
+
 export function App() {
   const { isLoading, error, data } = useQuery({
     queryKey: ['posts'],
@@ -58,6 +73,14 @@ export function App() {
     staleTime: 3_000,
   });
   const [selectedPostId, setSelectedPostId] = useState('');
+
+  const queryClient = useQueryClient();
+  const createPostMutation = useMutation({
+    mutationFn: createPost,
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ['posts'] });
+    },
+  });
 
   if (isLoading) return 'loading';
   if (error) return `error: ${error}`;
@@ -69,12 +92,27 @@ export function App() {
       <Post id={selectedPostId} />
     </>
   ) : (
-    <ul>
-      {data.map((post) => (
-        <li key={post.id} onClick={() => setSelectedPostId(post.id)}>
-          {post.title} ({post.views} views)
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {data.map((post) => (
+          <li key={post.id} onClick={() => setSelectedPostId(post.id)}>
+            {post.title} ({post.views} views)
+          </li>
+        ))}
+      </ul>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (!(e.target instanceof HTMLFormElement)) return;
+          const title = new FormData(e.target).get('title');
+          if (typeof title !== 'string' || title === '') return;
+
+          createPostMutation.mutate(title);
+        }}
+      >
+        <input name="title" />
+        <button type="submit">create</button>
+      </form>
+    </>
   );
 }
