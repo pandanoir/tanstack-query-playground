e954c98f8fa34e804a427fd18327a3fe880bf812
pandanoir <pandanoir@ymail.ne.jp>
2026-01-02 11:22:14 +0900

    課題 2：詳細画面(Query Key 設計)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
 import * as v from 'valibot';
 
 const PostSchema = v.object({
@@ -13,19 +14,63 @@ const fetchPosts = async () => {
   return v.parse(PostsSchema, await res.json());
 };
 
+const CommentSchema = v.object({
+  id: v.string(),
+  text: v.string(),
+  postId: v.string(),
+});
+const CommentsSchema = v.array(CommentSchema);
+
+const fetchComments = async (postId: string) => {
+  const url = new URL('http://localhost:3000/comments');
+  url.searchParams.append('postId', postId);
+  const res = await fetch(url.toString());
+  return v.parse(CommentsSchema, await res.json());
+};
+
+function Post({ id }: { id: string }) {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['comments', 'filter', id],
+    queryFn: () => fetchComments(id),
+    retry: false,
+  });
+
+  if (isLoading) return 'loading';
+  if (error) return `error: ${error}`;
+  if (!data) return 'loading';
+
+  return data.length > 0 ? (
+    <ul>
+      {data.map((comment) => (
+        <li key={comment.id}>{comment.text}</li>
+      ))}
+    </ul>
+  ) : (
+    <div>empty</div>
+  );
+}
+
 export function App() {
   const { isLoading, error, data } = useQuery({
     queryKey: ['posts'],
     queryFn: fetchPosts,
     retry: false,
   });
+  const [selectedPostId, setSelectedPostId] = useState('');
+
   if (isLoading) return 'loading';
   if (error) return `error: ${error}`;
   if (!data) return 'loading';
-  return (
+
+  return selectedPostId !== '' ? (
+    <>
+      <button onClick={() => setSelectedPostId('')}>back</button>
+      <Post id={selectedPostId} />
+    </>
+  ) : (
     <ul>
       {data.map((post) => (
-        <li key={post.id}>
+        <li key={post.id} onClick={() => setSelectedPostId(post.id)}>
           {post.title} ({post.views} views)
         </li>
       ))}
