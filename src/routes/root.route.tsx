2aaca6bfb7c4b74853faa5f5d6b885e68cae8280
pandanoir <pandanoir@ymail.ne.jp>
2026-01-02 19:28:21 +0900

    refactor

--- src/routes/root.route.tsx
+++ src/routes/root.route.tsx
@@ -3,10 +3,8 @@ import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
 export const rootRoute = createRootRoute({
   component: () => (
     <>
-      <nav style={{ marginBottom: '1rem' }}>
-        <Link to="/" style={{ marginRight: '1rem' }}>
-          Posts
-        </Link>
+      <nav style={{ display: 'flex', gap: '0.5em' }}>
+        <Link to="/">Posts</Link>
         <Link to="/notifications">Notifications</Link>
       </nav>
       <Outlet />
